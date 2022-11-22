f9f89ca02dec763d0e22195cd92ff64c2ed7f39a
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-11-22 09:57:16 +0100

    add job05 of day07

---
+++ jour07/job05/index.php
@@ -0,0 +1,20 @@
+<?php
+
+function occurences($str, $char)
+{
+      $i = 0;
+      $o = 0;
+      while (isset($str[$i]))
+      {
+            if ($char == $str[$i])
+            {
+                  $o++;
+            }
+            $i++;
+      }
+      return $o;
+}
+
+echo occurences('Bonjour','o');
+
+?>
