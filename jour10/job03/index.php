0e1f2b22156f4fe86e852deedc3cfa990c1f6716
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-11-25 12:00:01 +0100

    add job03 of day10

---
+++ jour10/job03/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$mysqli = new mysqli("localhost", "root", "", "jour09");
+
+$result = $mysqli->query("SELECT prenom,nom,naissance FROM etudiants WHERE sexe='Femme';");
+
+$row = $result -> fetch_array(MYSQLI_ASSOC);
+
+echo "<table border='1'><tr>";
+
+
+foreach ($row as $key => $value)
+{
+   echo " <th> " . $key . " </th> ";
+}
+echo "</tr>";
+while ($row != NULL)
+{
+   echo "<tr>";
+   foreach ($row as $value)
+   {
+         echo "<td>" . $value . "</td>";
+   }
+   echo "</tr>";
+   $row = $result -> fetch_array(MYSQLI_ASSOC);
+}
+echo "</table>";
+?>
+</body>
+</html>
